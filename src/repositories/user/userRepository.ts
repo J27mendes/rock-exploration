112a26925faad0fc908779abc19906e1e3e201ac
J27mendez <mendes_j27@hotmail.com>
2025-04-21 19:49:51 -0300

    feat: add repository for user creation

---
+++ src/repositories/user/userRepository.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma"
+
+type CreateBandParams = {
+  id?: string
+  banda: string
+  email: string
+  senha: string
+}
+
+export class PostgresCreateUserRepository {
+  async execute({ id, banda, email, senha }: CreateBandParams) {
+    const user = await prisma.user.create({
+      data: {
+        id,
+        banda,
+        email,
+        senha,
+      },
+    })
+
+    return user
+  }
+}
