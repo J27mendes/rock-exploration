55ddd548a1310c8f8d29498121dbfd2489790016
J27mendez <mendes_j27@hotmail.com>
2025-04-22 12:11:58 -0300

    feat: returned  istance of errors correctly

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -9,10 +9,16 @@ export async function POST(req: NextRequest) {
     const user = await controller.execute(body)
 
     return NextResponse.json(user, { status: 201 })
-  } catch (error: any) {
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: error.message || "Erro ao criar usuário" },
+        { status: 400 }
+      )
+    }
     return NextResponse.json(
-      { error: error.message || "Erro ao criar usuário" },
-      { status: 400 }
+      { error: "Erro desconhecido ao criar usuário" },
+      { status: 500 }
     )
   }
 }
