e5bea6566e888845366f40e0eea698ae432f449e
J27mendez <mendes_j27@hotmail.com>
2025-04-22 16:07:13 -0300

    feat: add password comparator adapter

---
+++ src/adapters/passwordComparatorAdapter.ts
@@ -0,0 +1,6 @@
+import bcrypt from "bcrypt"
+export class PasswordComparatorAdapter {
+  async execute(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword)
+  }
+}
