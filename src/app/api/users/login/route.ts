fc93940161ff406a275ff8554f3a9144a0add70e
J27mendez <mendes_j27@hotmail.com>
2025-04-22 19:06:19 -0300

    feat: simplifying the httpResponse and ensuring there are no extra fields at login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from "next/server"
 import { LoginUserController } from "@/controller/user/loginUserController"
+import { validateLoginFields } from "@/utils/validateLoginFields"
+import { badRequest, serverError } from "@/helpers/httpResponse"
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
+
+    validateLoginFields(body)
+
     const controller = new LoginUserController()
     const result = await controller.execute(body)
 
     return NextResponse.json(result, { status: 200 })
   } catch (error: unknown) {
     if (error instanceof Error) {
-      return NextResponse.json(
-        { error: error.message || "Erro ao logar usuário" },
-        { status: 400 }
-      )
+      return badRequest(error.message)
     }
-    return NextResponse.json(
-      { error: "Erro desconhecido ao logar usuário" },
-      { status: 500 }
-    )
+
+    return serverError(error)
   }
 }
