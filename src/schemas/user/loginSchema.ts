72c15fd119c1df9fb7e3e7dbb230149e7c14bd3e
J27mendez <mendes_j27@hotmail.com>
2025-04-22 16:08:00 -0300

    feat: add login user schema

---
+++ src/schemas/user/loginSchema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod"
+
+export const loginUserSchema = z.object({
+  email: z.string().email("E-mail inválido").trim(),
+  senha: z.string().min(8, "A senha precisa ter no mínimo 8 caracteres").trim(),
+})
