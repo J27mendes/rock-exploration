821944abad867cc72d81dd66183eba36e75e2b48
J27mendez <mendes_j27@hotmail.com>
2025-04-23 20:40:26 -0300

    feat: add method to compare passwords

--- src/adapters/passwordHasherAdapter.ts
+++ src/adapters/passwordHasherAdapter.ts
@@ -1,7 +1,13 @@
 import bcrypt from "bcrypt"
 
 export class PasswordHasherAdapter {
+  private readonly saltRounds = 10
+
   async hash(password: string): Promise<string> {
-    return bcrypt.hash(password, 10)
+    return bcrypt.hash(password, this.saltRounds)
+  }
+
+  async compare(plainText: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(plainText, hashed)
   }
 }
