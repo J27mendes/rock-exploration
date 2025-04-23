3980e73eca75a9072571d422d37b0c2e12ac6dda
J27mendez <mendes_j27@hotmail.com>
2025-04-23 20:49:15 -0300

    feat: add strict at  loginUserSchema

--- src/schemas/user/loginSchema.ts
+++ src/schemas/user/loginSchema.ts
@@ -1,6 +1,11 @@
 import { z } from "zod"
 
-export const loginUserSchema = z.object({
-  email: z.string().email("E-mail inválido").trim(),
-  senha: z.string().min(8, "A senha precisa ter no mínimo 8 caracteres").trim(),
-})
+export const loginUserSchema = z
+  .object({
+    email: z.string().email("E-mail inválido").trim(),
+    senha: z
+      .string()
+      .min(8, "A senha precisa ter no mínimo 8 caracteres")
+      .trim(),
+  })
+  .strict()
