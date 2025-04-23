58f2d98b3fa5ec1ed43ea1abbfbbc9312740d0af
J27mendez <mendes_j27@hotmail.com>
2025-04-23 20:58:28 -0300

    feat: add middleware to authorization tokens

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+import { env } from "@/lib/envs"
+import { unauthorized } from "@/helpers/httpResponse"
+
+export function authorization(req: Request): string | Response {
+  const authHeader = req.headers.get("authorization")
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return unauthorized("Token not provided")
+  }
+
+  const token = authHeader.replace("Bearer ", "").trim()
+
+  try {
+    const decoded = jwt.verify(token, env.JWT_ACCESS_TOKEN_SECRET) as {
+      userId: string
+    }
+
+    return decoded.userId
+  } catch (error) {
+    console.error("JWT verification failed:", error)
+    return unauthorized("Invalid or expired token")
+  }
+}
