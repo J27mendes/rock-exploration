4a8d6d58995c1abfb7683c6a9d636a2223130eac
J27mendez <mendes_j27@hotmail.com>
2025-04-23 20:57:09 -0300

    feat: add UpdateUserUseCase

---
+++ src/useCases/user/updateUseCase.ts
@@ -0,0 +1,62 @@
+import { PasswordHasherAdapter } from "@/adapters/passwordHasherAdapter"
+import { conflict } from "@/helpers/httpResponse"
+import { prisma } from "@/lib/prisma"
+
+interface UpdateUserInput {
+  userId: string
+  data: {
+    email?: string
+    senha?: string
+  }
+}
+
+export class UpdateUserUseCase {
+  async execute({ userId, data }: UpdateUserInput) {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { email: true, senha: true },
+    })
+
+    if (!user) {
+      return conflict("Usuário não encontrado.")
+    }
+
+    const hasher = new PasswordHasherAdapter()
+    const emailIgual = data.email ? data.email === user.email : true
+    const senhaIgual = data.senha
+      ? await hasher.compare(data.senha, user.senha)
+      : true
+
+    if (emailIgual && senhaIgual) {
+      return conflict(
+        "Você precisa alterar ao menos um dos campos para atualizar."
+      )
+    }
+
+    if (data.email && !emailIgual) {
+      const emailExists = await prisma.user.findFirst({
+        where: {
+          email: data.email,
+          NOT: { id: userId },
+        },
+      })
+
+      if (emailExists) {
+        return conflict("E-mail já está em uso por outro usuário.")
+      }
+    }
+
+    if (data.senha && !senhaIgual) {
+      data.senha = await hasher.hash(data.senha)
+    } else {
+      delete data.senha
+    }
+
+    const updated = await prisma.user.update({
+      where: { id: userId },
+      data,
+    })
+
+    return updated
+  }
+}
