dd196d8d1746ca6ebe6da6233bbf3b709a2bac11
J27mendez <mendes_j27@hotmail.com>
2025-04-23 21:02:08 -0300

    refactor:  using helpers and leaving the validation of extra fields in the creation route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,22 +1,21 @@
 import { NextRequest } from "next/server"
 import { PostUserController } from "@/controller/user/userController"
-import { badRequest, created, serverError } from "@/helpers/httpResponse"
-import { validateSignupFields } from "@/utils/validateSignupFields"
+import { created, handleZodError, serverError } from "@/helpers/httpResponse"
+import { ZodError } from "zod"
+import { createUserSchema } from "@/schemas/user/userSchema"
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
-
-    validateSignupFields(body)
-
+    const validatedData = await createUserSchema.parseAsync(body)
     const controller = new PostUserController()
-    const result = await controller.execute(body)
+    const result = await controller.execute(validatedData)
 
     const response = created(result)
     return response
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return badRequest(error.message)
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return handleZodError(error)
     }
 
     return serverError(error)
