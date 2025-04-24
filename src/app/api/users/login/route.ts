d30f327cea6ceb6ea11eebaf02091cca8852c1f0
J27mendez <mendes_j27@hotmail.com>
2025-04-23 21:00:30 -0300

    refactor: using helpers  and validation of extra fields in the login route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,21 +1,20 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 import { LoginUserController } from "@/controller/user/loginUserController"
-import { validateLoginFields } from "@/utils/validateLoginFields"
-import { badRequest, serverError } from "@/helpers/httpResponse"
+import { handleZodError, ok, serverError } from "@/helpers/httpResponse"
+import { loginUserSchema } from "@/schemas/user/loginSchema"
+import { ZodError } from "zod"
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
-
-    validateLoginFields(body)
-
+    const validationResult = await loginUserSchema.parseAsync(body)
     const controller = new LoginUserController()
-    const result = await controller.execute(body)
+    const result = await controller.execute(validationResult)
 
-    return NextResponse.json(result, { status: 200 })
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return badRequest(error.message)
+    return ok(result)
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return handleZodError(error)
     }
 
     return serverError(error)
