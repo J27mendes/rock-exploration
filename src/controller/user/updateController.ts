979fc013f779b8709b996aa8c1b433ffaf438215
J27mendez <mendes_j27@hotmail.com>
2025-04-24 19:04:05 -0300

    feat: ensuring that the updated user comes with the tokens

--- src/controller/user/updateController.ts
+++ src/controller/user/updateController.ts
@@ -2,18 +2,40 @@ import { updateUserSchema } from "@/schemas/user/updateSchema"
 import { z, ZodError } from "zod"
 import { UpdateUserUseCase } from "@/useCases/user/updateUseCase"
 import { badRequest, serverError } from "@/helpers/httpResponse"
+import { TokensGenerator } from "@/adapters/tokensGeneratorAdapter"
+import { NextResponse } from "next/server"
 
 export type UpdateUserDTO = z.infer<typeof updateUserSchema>
 
+type SuccessUpdate = {
+  id: string
+  email: string
+  senha: string
+  banda: string
+  createdAt: Date
+  updatedAt: Date
+}
+
 export class UpdateUserController {
   private useCase = new UpdateUserUseCase()
+  private tokensGenerator = new TokensGenerator()
 
   async execute(body: UpdateUserDTO, userId: string) {
     try {
       const validated = updateUserSchema.parse(body)
       const result = await this.useCase.execute({ userId, data: validated })
 
-      return result
+      if (result instanceof NextResponse) {
+        return result
+      }
+
+      const user = result as SuccessUpdate
+      const tokens = this.tokensGenerator.execute(user.id)
+
+      return {
+        user,
+        tokens,
+      }
     } catch (error) {
       if (error instanceof ZodError) {
         return badRequest(error.errors)
