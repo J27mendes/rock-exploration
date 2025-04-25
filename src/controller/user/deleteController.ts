58de2abd8bc284e3eaf35328dee9bf08e4f1912a
J27mendez <mendes_j27@hotmail.com>
2025-04-24 23:10:27 -0300

    feat: add delete controller

---
+++ src/controller/user/deleteController.ts
@@ -0,0 +1,20 @@
+import { DeleteUserUseCase } from "@/useCases/user/deleteUseCase"
+import { unauthorized } from "@/helpers/httpResponse"
+export class DeleteUserController {
+  private useCase = new DeleteUserUseCase()
+
+  async execute(userId: string, confirmMessage: string) {
+    if (confirmMessage !== "CONFIRMO A EXCLUSÃO DA CONTA") {
+      return unauthorized(
+        "Confirmação inválida. Digite exatamente: CONFIRMO A EXCLUSÃO DA CONTA"
+      )
+    }
+
+    const deletedUser = await this.useCase.execute(userId)
+
+    return {
+      message: "Usuário deletado com sucesso",
+      deleted: deletedUser,
+    }
+  }
+}
