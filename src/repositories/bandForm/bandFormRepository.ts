ef265307989a15b37c519e604dc857562bb75de6
J27mendez <mendes_j27@hotmail.com>
2025-04-26 21:19:30 -0300

    feat: add initial CreateBandFormRepository

---
+++ src/repositories/bandForm/bandFormRepository.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/prisma"
+import { BandForm } from "@prisma/client"
+import { CreateBandFormDTO } from "@/useCases"
+import { generateId } from "@/utils"
+
+export class CreateBandFormRepository {
+  async findByUserId(userId: string): Promise<BandForm | null> {
+    try {
+      const bandForm = await prisma.bandForm.findUnique({
+        where: { userId },
+      })
+      return bandForm
+    } catch (error) {
+      console.error(`Erro no banco de dados ao buscar formulário! ${error}`)
+      throw new Error("Erro no banco de dados ao buscar formulário!.")
+    }
+  }
+
+  async create(data: CreateBandFormDTO, userId: string): Promise<BandForm> {
+    try {
+      const newBandForm = await prisma.bandForm.create({
+        data: {
+          id: generateId(),
+          idBanda: userId,
+          banda: data.banda,
+          quantidadeIntegrantes: data.quantidadeIntegrantes,
+          integrantes: data.integrantes,
+          estilo: data.estilo,
+          release: data.release,
+          imagem: data.imagem,
+          quantidadeMusicas: data.quantidadeMusicas,
+          setList: data.setList,
+          contato: data.contato,
+          tempoApresentacao: data.tempoApresentacao,
+          user: {
+            connect: { id: userId },
+          },
+        },
+      })
+      return newBandForm
+    } catch (error) {
+      console.error("Erro ao criar formulário no banco:", error)
+      throw new Error("Erro ao criar formulário")
+    }
+  }
+}
