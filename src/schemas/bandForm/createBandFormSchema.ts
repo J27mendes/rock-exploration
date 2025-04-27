e870f61cfec332880cf531daaece924226791030
J27mendez <mendes_j27@hotmail.com>
2025-04-26 21:52:56 -0300

    feat: add strict at createBandFormSchema

--- src/schemas/bandForm/createBandFormSchema.ts
+++ src/schemas/bandForm/createBandFormSchema.ts
@@ -1,55 +1,59 @@
 import { z } from "zod"
 
-export const createBandFormSchema = z.object({
-  banda: z.string().min(1, "Nome da banda é obrigatório"),
-  quantidadeIntegrantes: z
-    .number()
-    .int()
-    .min(1, "Quantidade de integrantes é obrigatória"),
+export const createBandFormSchema = z
+  .object({
+    banda: z.string().min(1, "Nome da banda é obrigatório"),
+    quantidadeIntegrantes: z
+      .number()
+      .int()
+      .min(1, "Quantidade de integrantes é obrigatória"),
 
-  integrantes: z.array(
-    z.object({
-      nome: z.string().min(1, "Nome do integrante é obrigatório"),
-      instrumento: z.string().min(1, "Instrumento do integrante é obrigatório"),
-    })
-  ),
+    integrantes: z.array(
+      z.object({
+        nome: z.string().min(1, "Nome do integrante é obrigatório"),
+        instrumento: z
+          .string()
+          .min(1, "Instrumento do integrante é obrigatório"),
+      })
+    ),
 
-  estilo: z.string().min(1, "Estilo musical é obrigatório"),
-  release: z.string().min(1, "Release é obrigatório"),
+    estilo: z.string().min(1, "Estilo musical é obrigatório"),
+    release: z.string().min(1, "Release é obrigatório"),
 
-  imagem: z.object({
-    urlImagemBanda: z.string().url("URL da imagem da banda inválida"),
-    urlImagemLogo: z.string().url("URL do logo inválida"),
-    urlMapaPalco: z.string().url("URL do mapa de palco inválida"),
-  }),
+    imagem: z.object({
+      urlImagemBanda: z.string().url("URL da imagem da banda inválida"),
+      urlImagemLogo: z.string().url("URL do logo inválida"),
+      urlMapaPalco: z.string().url("URL do mapa de palco inválida"),
+    }),
 
-  quantidadeMusicas: z
-    .number()
-    .int()
-    .min(1, "Quantidade de músicas é obrigatória"),
+    quantidadeMusicas: z
+      .number()
+      .int()
+      .min(1, "Quantidade de músicas é obrigatória"),
 
-  setList: z.array(
-    z.object({
-      nomeMusica: z.string().min(1, "Nome da música é obrigatório"),
-      tempoMusica: z.number().int().min(1, "Tempo da música é obrigatório"),
-      letraMusica: z.string().min(1, "Letra da música é obrigatória"),
-    })
-  ),
+    setList: z.array(
+      z.object({
+        nomeMusica: z.string().min(1, "Nome da música é obrigatório"),
+        tempoMusica: z.number().int().min(1, "Tempo da música é obrigatório"),
+        letraMusica: z.string().min(1, "Letra da música é obrigatória"),
+      })
+    ),
 
-  contato: z.object({
-    email: z.string().email("Email inválido"),
-    nomePrimeiroNumero: z
-      .string()
-      .min(1, "Nome do primeiro número é obrigatório"),
-    primeiroNumero: z.string().min(1, "Primeiro número é obrigatório"),
-    nomeSegundoNumero: z
-      .string()
-      .min(1, "Nome do segundo número é obrigatório"),
-    segundoNumero: z.string().min(1, "Segundo número é obrigatório"),
-  }),
+    contato: z.object({
+      email: z.string().email("Email inválido"),
+      nomePrimeiroNumero: z
+        .string()
+        .min(1, "Nome do primeiro número é obrigatório"),
+      primeiroNumero: z.string().min(1, "Primeiro número é obrigatório"),
+      nomeSegundoNumero: z
+        .string()
+        .min(1, "Nome do segundo número é obrigatório"),
+      segundoNumero: z.string().min(1, "Segundo número é obrigatório"),
+    }),
 
-  tempoApresentacao: z
-    .number()
-    .int()
-    .min(1, "Tempo de apresentação é obrigatório"),
-})
+    tempoApresentacao: z
+      .number()
+      .int()
+      .min(1, "Tempo de apresentação é obrigatório"),
+  })
+  .strict()
