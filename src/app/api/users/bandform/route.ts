80189d619a2ef19d5c6c2c654b4b098f937ad4cb
J27mendez <mendes_j27@hotmail.com>
2025-04-29 17:10:46 -0300

    feat: add image upload to route

--- src/app/api/users/bandform/route.ts
+++ src/app/api/users/bandform/route.ts
@@ -4,20 +4,70 @@ import { authorization } from "@/middleware/authMiddleware"
 import { ok, serverError, handleZodError } from "@/helpers/httpResponse"
 import { NextRequest } from "next/server"
 import { ZodError } from "zod"
+import { uploadImages } from "@/utils"
+interface UploadFile {
+  buffer: Buffer
+  originalname: string
+  mimetype: string
+}
 
 export async function POST(req: NextRequest) {
   try {
     const tokenValidate = authorization(req)
-
     if (tokenValidate instanceof Response) {
       return tokenValidate
     }
 
     const idBandForm = tokenValidate
-    const body = await req.json()
-    const validatedBody = await createBandFormSchema.parseAsync(body)
+
+    const formData = await req.formData()
+
+    const textFields: Record<string, string> = {}
+    const files: UploadFile[] = []
+
+    for (const [key, value] of formData.entries()) {
+      if (value instanceof File) {
+        files.push({
+          buffer: Buffer.from(await value.arrayBuffer()),
+          originalname: value.name,
+          mimetype: value.type,
+        })
+      } else {
+        textFields[key] = value
+      }
+    }
+
+    const bodyParcial = {
+      ...textFields,
+      quantidadeIntegrantes: Number(textFields.quantidadeIntegrantes),
+      quantidadeMusicas: Number(textFields.quantidadeMusicas),
+      integrantes: JSON.parse(textFields.integrantes),
+      setList: JSON.parse(textFields.setList),
+      contato: JSON.parse(textFields.contato),
+    }
+
+    if (files.length !== 3) {
+      return new Response(
+        JSON.stringify({ error: "Você precisa enviar exatamente 3 imagens." }),
+        { status: 400 }
+      )
+    }
+
+    const urls = await uploadImages(files, textFields.banda)
+
+    const fullBbody = {
+      ...bodyParcial,
+      imagem: {
+        urlImagemBanda: urls.urlImagemBanda,
+        urlImagemLogo: urls.urlImagemLogo,
+        urlMapaPalco: urls.urlMapaPalco,
+      },
+    }
+
+    const validatedBody = await createBandFormSchema.parseAsync(fullBbody)
     const controller = new CreateBandFormController()
-    const response = await controller.execute(req, validatedBody, idBandForm)
+    const response = await controller.execute(validatedBody, idBandForm)
+
     if (response instanceof Response) {
       return response
     }
@@ -27,7 +77,7 @@ export async function POST(req: NextRequest) {
     if (error instanceof ZodError) {
       return handleZodError(error)
     }
-
+    console.error("Erro:", error)
     return serverError(error)
   }
 }
