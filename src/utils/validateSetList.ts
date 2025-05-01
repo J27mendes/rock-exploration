15d73d1e9bf86e0c59a8a057272f1ec4fa9f13c9
J27mendez <mendes_j27@hotmail.com>
2025-05-01 12:28:52 -0300

    feat: add validation to total temp setlist

---
+++ src/utils/validateSetList.ts
@@ -0,0 +1,12 @@
+import { calculateTotalMusicTime } from "./calculateTotalMusicTime"
+import { BadRequestError } from "@/errors"
+
+export function validateAndCalculateSetList(setList: any[]) {
+  const totalTime = calculateTotalMusicTime(setList)
+  if (totalTime < 2400 || totalTime > 3600) {
+    throw new BadRequestError(
+      "O total do tempo das músicas deve estar entre 2400 e 3600 segundos."
+    )
+  }
+  return totalTime
+}
