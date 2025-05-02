1492092587c1c44f796f8dc90949a510a1b41338
J27mendez <mendes_j27@hotmail.com>
2025-05-02 18:30:35 -0300

    refactor: add handleSimpleFieldUpdates to update simple fields

---
+++ src/utils/handleSimpleFieldUpdates.ts
@@ -0,0 +1,63 @@
+import { formattedBandName } from "./formatBandName"
+import { formatStyle } from "./formatStyle"
+import { updateBandFormSchema } from "@/schemas"
+import { z } from "zod"
+
+type UpdateBandFormDTO = z.infer<typeof updateBandFormSchema>
+
+export function handleSimpleFieldUpdates<T extends keyof UpdateBandFormDTO>(
+  updates: Partial<UpdateBandFormDTO>,
+  field: T,
+  newValue: UpdateBandFormDTO[T],
+  oldValue: UpdateBandFormDTO[T]
+) {
+  if (newValue !== undefined && newValue !== oldValue) {
+    updates[field] = newValue
+  }
+}
+
+export function updateBandForm(
+  validatedData: UpdateBandFormDTO,
+  existingForm: UpdateBandFormDTO
+) {
+  const updates: Partial<UpdateBandFormDTO> = {}
+
+  if (validatedData.banda) {
+    const bandName = formattedBandName(validatedData.banda)
+    if (bandName !== existingForm.banda) {
+      updates.banda = bandName
+    }
+  }
+
+  if (validatedData.estilo) {
+    handleSimpleFieldUpdates(
+      updates,
+      "estilo",
+      formatStyle(validatedData.estilo),
+      existingForm.estilo
+    )
+  }
+
+  handleSimpleFieldUpdates(
+    updates,
+    "release",
+    validatedData.release,
+    existingForm.release
+  )
+
+  handleSimpleFieldUpdates(
+    updates,
+    "quantidadeIntegrantes",
+    validatedData.quantidadeIntegrantes,
+    existingForm.quantidadeIntegrantes
+  )
+
+  handleSimpleFieldUpdates(
+    updates,
+    "quantidadeMusicas",
+    validatedData.quantidadeMusicas,
+    existingForm.quantidadeMusicas
+  )
+
+  return updates
+}
