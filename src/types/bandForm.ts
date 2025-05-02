649d94119844586931ddb31816c4148160064ad8
J27mendez <mendes_j27@hotmail.com>
2025-05-02 18:33:05 -0300

    feat: add type to image

--- src/types/bandForm.ts
+++ src/types/bandForm.ts
@@ -5,3 +5,7 @@ import { z } from "zod"
 export type UpdateBandFormDTO = z.infer<typeof updateBandFormSchema>
 
 export type Music = CreateBandFormDTO["setList"][number]
+
+export type Imagem = NonNullable<
+  z.infer<typeof updateBandFormSchema.shape.imagem>
+>
