266f24ac2172cd711295ce230dedaf2d08d5655c
J27mendez <mendes_j27@hotmail.com>
2025-05-08 17:08:44 -0300

    feat: add ScrollText component

---
+++ src/components/ScrollText.tsx
@@ -0,0 +1,26 @@
+const ScrollText = () => {
+  return (
+    <div className="animate-scrollStarWars space-y-6 text-2xl font-semibold text-white/80">
+      <p>Sua banda tem talento, mas ninguém conseguem ouvir sua mensagem?</p>
+      <p>As letras se perdem nos shows?</p>
+      <p>Falta visibilidade, apoio ou espaço para mostrar sua arte?</p>
+      <p>
+        O Rock Exploration chegou para mudar esse cenário e tirar sua banda das
+        cavernas.
+      </p>
+      <p>
+        Unimos tecnologia e música para dar voz às bandas independentes, clareza
+        às suas letras e palco à sua história.
+      </p>
+      <p>
+        Se você é artista ou fã do rock local, ou simplesmente ama o rock, essa
+        experiência é feita pra você.
+      </p>
+      <p className="animate-flash text-3xl text-white">
+        Rock Exploration — Tecnologia em Sintonia com a Música.
+      </p>
+    </div>
+  )
+}
+
+export default ScrollText
