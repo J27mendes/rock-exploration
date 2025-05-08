15dd2d23bbc4cb7a42d85536398a9cf2d42d00ff
J27mendez <mendes_j27@hotmail.com>
2025-05-08 17:11:29 -0300

    feat: add initial for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import Button from "@/components/Button"
+import ScrollText from "@/components/ScrollText"
+
 const Home = () => {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-amber-100 p-8 pb-20 font-[family-name:var(--font-geist-sans)] opacity-75 sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] text-8xl sm:items-start">
-        Rock & Exploration
-      </main>
+    <div className="relative flex min-h-screen flex-col items-center justify-end overflow-hidden bg-[#1a1a1a] bg-hero-image">
+      <div className="relative z-0 mt-8 h-[300px] w-[90%] max-w-[600px] overflow-hidden rounded-lg p-4 text-center shadow-lg backdrop-blur">
+        <ScrollText />
+      </div>
+      <div className="flex w-full justify-around pb-4">
+        <Button label="Login" />
+        <Button label="Criar conta" bgColor="#1695c0" className="font-medium" />
+      </div>
     </div>
   )
 }
