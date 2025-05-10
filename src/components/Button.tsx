7618a3fefba727bc54130effe70556e86242b7b3
J27mendez <mendes_j27@hotmail.com>
2025-05-10 12:43:54 -0300

    refactor: add atributes in the button and children

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,26 @@
 type CustomButtonProps = {
-  label: string
+  children?: React.ReactNode
   bgColor?: string
   onClick?: () => void
   className?: string
+  type?: "button" | "submit" | "reset"
 }
 
 const Button = ({
-  label,
+  children,
   bgColor = "#d2783b",
   onClick,
   className = "",
+  type = "button",
 }: CustomButtonProps) => {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className={`animate-pulseStrong h-16 w-32 rounded-lg border-4 border-white text-xl font-semibold text-white ${className}`}
+      className={`h-16 w-32 animate-pulseStrong rounded-lg border-4 border-white text-xl font-semibold text-white ${className}`}
       style={{ backgroundColor: bgColor }}
     >
-      {label}
+      {children}
     </button>
   )
 }
