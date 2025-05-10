b3a5f96bf63ba39f8fb3916655b5ba425ca6d161
J27mendez <mendes_j27@hotmail.com>
2025-05-10 11:11:29 -0300

    feat: add userService

---
+++ src/app/api/services/user.ts
@@ -0,0 +1,31 @@
+import { UpdateUserInput } from "@/interfaces"
+import { protectedApi, publicApi } from "@/lib/axios"
+import { CreateUserInput } from "@/types"
+
+export const UserService = {
+  signup: async (input: CreateUserInput) => {
+    const response = await publicApi.post("/users", {
+      banda: input.banda,
+      email: input.email,
+      senha: input.senha,
+    })
+    return response.data
+  },
+
+  me: async () => {
+    const response = await protectedApi.get("/users/me")
+    return response.data
+  },
+
+  updateMe: async (input: UpdateUserInput) => {
+    const response = await protectedApi.patch("/users/me", input)
+    return response.data
+  },
+
+  deleteMe: async (confirmMessage: string) => {
+    const response = await protectedApi.delete("/users/me", {
+      data: { confirmMessage },
+    })
+    return response.data
+  },
+}
