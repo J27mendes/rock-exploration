88d54f05d31de91843f3bf44fdbbdfd80e751942
J27mendez <mendes_j27@hotmail.com>
2025-05-11 18:28:44 -0300

    feat: add onSuccess and onError

--- src/app/api/hooks/user.ts
+++ src/app/api/hooks/user.ts
@@ -1,17 +1,30 @@
 import { useMutation } from "@tanstack/react-query"
 
+import { showError, showSuccess } from "@/lib/toast"
 import { CreateUserInput } from "@/types"
 
 import { UserService } from "../services/user"
 
 export const mutationSignupKey = ["signup"]
 
-export const useSignup = () => {
-  return useMutation({
+export const useSignup = (onSuccessCallback?: () => void) => {
+  return useMutation<unknown, Error, CreateUserInput>({
     mutationKey: mutationSignupKey,
     mutationFn: async (variables: CreateUserInput) => {
-      const response = UserService.signup(variables)
+      const response = await UserService.signup(variables)
       return response
+    },
+    onSuccess: () => {
+      showSuccess("Usuário criado com sucesso!")
+      if (onSuccessCallback) onSuccessCallback()
+    },
+    onError: (error: any) => {
+      console.error("Erro ao criar usuário:", error)
+      if (error.response?.status === 409) {
+        showError("Este e-mail já está em uso.")
+      } else {
+        showError("Falha ao criar usuário.")
+      }
     },
   })
 }
