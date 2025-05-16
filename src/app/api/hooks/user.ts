d6dc1db29916c433b64c1aa9bdb42e747f79d62d
J27mendez <mendes_j27@hotmail.com>
2025-05-16 16:48:36 -0300

    feat: add useLogin hook

--- src/app/api/hooks/user.ts
+++ src/app/api/hooks/user.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query"
 
 import { showError, showSuccess } from "@/lib/toast"
-import type { UserWithTokens } from "@/types"
+import type { LoginUserDTO, UserWithTokens } from "@/types"
 import { CreateUserInput } from "@/types"
 
 import { UserService } from "../services/user"
@@ -34,6 +34,37 @@ export const useSignup = (
       } else {
         showError("Falha ao criar usuário.")
       }
+    },
+  })
+}
+
+export const mutationLoginKey = ["login"]
+
+export const useLogin = (
+  onSuccessCallback?: (loginUser: UserWithTokens) => void,
+) => {
+  return useMutation<UserWithTokens, Error, LoginUserDTO>({
+    mutationKey: mutationSignupKey,
+    mutationFn: async (variables: LoginUserDTO) => {
+      const response = await UserService.login(variables)
+      return response
+    },
+    onSuccess: (loginUser) => {
+      showSuccess("Usuário logado com sucesso!")
+      if (onSuccessCallback) onSuccessCallback(loginUser)
+    },
+    onError: (error: any) => {
+      console.error("Erro ao logar usuário:", error)
+
+      if (error.response?.status === 401) {
+        return showError("E-mail ou senha estão incorretos.")
+      }
+
+      if (error.response?.status === 404) {
+        return showError("Usuário não encontrado.")
+      }
+
+      return showError("Erro inesperado ao logar.")
     },
   })
 }
