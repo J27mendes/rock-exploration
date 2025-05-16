3e679fe5eb646676f39f401189745aa8933e3ebe
J27mendez <mendes_j27@hotmail.com>
2025-05-16 16:42:09 -0300

    feat: add showLogin and UserLogin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import { useState } from "react"
 
 import Button from "@/components/Button"
+import UserLogin from "@/components/LoginDialog"
 import ScrollText from "@/components/ScrollText"
 import CreateSignup from "@/components/SignupDialog"
 
 const Home = () => {
   const [showSignup, setShowSignup] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
 
   const handleClose = () => {
     setShowSignup(false)
@@ -18,7 +20,13 @@ const Home = () => {
         <ScrollText />
       </div>
       <div className="flex w-full justify-around pb-4">
-        <Button>Login</Button>
+        <Button
+          onClick={() => {
+            setShowLogin(true)
+          }}
+        >
+          {showLogin ? "Aguardando" : "Login"}
+        </Button>
         <Button
           bgColor="#1695c0"
           onClick={() => {
@@ -29,6 +37,27 @@ const Home = () => {
           {showSignup ? "Aguardando" : "Criar conta"}
         </Button>
       </div>
+      {showLogin && (
+        <div className="absolute inset-0 z-50 flex items-center justify-center">
+          <div className="w-full max-w-md rounded-lg bg-black bg-opacity-50 p-6">
+            <p className="flex justify-center font-semibold text-white">
+              Rock Exploration
+            </p>
+            <UserLogin onClose={handleClose} />
+
+            <div className="mt-4 flex justify-end">
+              <Button
+                onClick={() => setShowLogin(false)}
+                bgColor="#999"
+                type="button"
+                className="h-12"
+              >
+                Fechar
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
 
       {showSignup && (
         <div className="absolute inset-0 z-50 flex items-center justify-center">
