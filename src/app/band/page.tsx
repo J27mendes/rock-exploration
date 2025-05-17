ee07118ae7afde6788cbe9423657254247c31e5b
J27mendez <mendes_j27@hotmail.com>
2025-05-17 15:11:08 -0300

    feat: add initial page bandPage

---
+++ src/app/band/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import ImageComponent from "@/components/ImageComponent"
+
+const BandPage = () => {
+  return (
+    <ImageComponent>
+      <div className="justify-items-start text-yellow-300">olá banda</div>
+    </ImageComponent>
+  )
+}
+
+export default BandPage
