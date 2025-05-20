26eb8c7b2772053fbcb04b33420b85a4e945abdb
J27mendez <mendes_j27@hotmail.com>
2025-05-20 16:49:13 -0300

    refactor: decreasing the indentation size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,103 +1,103 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-  	borderRadius: {
-  		DEFAULT: 'var(--radius)',
-  		md: 'calc(var(--radius) - 2px)',
-  		lg: 'calc(var(--radius) + 2px)'
-  	},
-  	backgroundImage: {
-  		'hero-image': 'var(--image)'
-  	},
-  	extends: {
-  		colors: {
-  			destructive: 'hsl(var(--destructive))'
-  		}
-  	},
-  	keyframes: {
-  		flash: {
-  			'0%, 100%': {
-  				opacity: '1'
-  			},
-  			'50%': {
-  				opacity: '0.2'
-  			}
-  		},
-  		pulseStrong: {
-  			'0%, 100%': {
-  				transform: 'scale(1)',
-  				opacity: '1'
-  			},
-  			'50%': {
-  				transform: 'scale(1.1)',
-  				opacity: '0.7'
-  			}
-  		},
-  		scrollStarWars: {
-  			'0%': {
-  				transform: 'rotateX(25deg) translateY(100%)'
-  			},
-  			'100%': {
-  				transform: 'rotateX(25deg) translateY(-300%)'
-  			}
-  		}
-  	},
-  	animation: {
-  		flash: 'flash 2s infinite',
-  		pulseStrong: 'pulseStrong 3s ease-in-out infinite',
-  		scrollStarWars: 'scrollStarWars 90s linear infinite'
-  	},
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    borderRadius: {
+      DEFAULT: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      lg: "calc(var(--radius) + 2px)",
+    },
+    backgroundImage: {
+      "hero-image": "var(--image)",
+    },
+    extends: {
+      colors: {
+        destructive: "hsl(var(--destructive))",
+      },
+    },
+    keyframes: {
+      flash: {
+        "0%, 100%": {
+          opacity: "1",
+        },
+        "50%": {
+          opacity: "0.2",
+        },
+      },
+      pulseStrong: {
+        "0%, 100%": {
+          transform: "scale(1)",
+          opacity: "1",
+        },
+        "50%": {
+          transform: "scale(1.1)",
+          opacity: "0.7",
+        },
+      },
+      scrollStarWars: {
+        "0%": {
+          transform: "rotateX(25deg) translateY(100%)",
+        },
+        "100%": {
+          transform: "rotateX(25deg) translateY(-300%)",
+        },
+      },
+    },
+    animation: {
+      flash: "flash 2s infinite",
+      pulseStrong: "pulseStrong 3s ease-in-out infinite",
+      scrollStarWars: "scrollStarWars 90s linear infinite",
+    },
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
