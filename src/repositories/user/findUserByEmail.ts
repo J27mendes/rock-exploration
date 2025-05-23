654dde0568e7db7bc5f4631346c8c6daad8d2d07
J27mendez <mendes_j27@hotmail.com>
2025-05-22 23:09:06 -0300

    refactor: add await int the return

--- src/repositories/user/findUserByEmail.ts
+++ src/repositories/user/findUserByEmail.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma"
 
 export class FindUserByEmailRepository {
   async execute(email: string) {
-    return prisma.user.findUnique({
+    return await prisma.user.findUnique({
       where: { email },
     })
   }
