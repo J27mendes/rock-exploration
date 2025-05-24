eb59f02383cc1a8a565d73febf70a9de2174b5ac
J27mendez <mendes_j27@hotmail.com>
2025-05-23 23:14:42 -0300

    feat: add interfaces Band, BandProps, IdBand

--- src/interfaces/bandForm.ts
+++ src/interfaces/bandForm.ts
@@ -38,3 +38,32 @@ export interface UploadFile {
 export interface FormWithId {
   id: string
 }
+export interface Band {
+  id: string
+  banda: string
+  quantidadeIntegrantes: number
+  integrantes: { nome: string; instrumento: string }[]
+  estilo: string
+  release: string
+  imagem: {
+    urlImagemBanda: string
+    urlImagemLogo: string
+    urlMapaPalco: string
+  }
+  quantidadeMusicas: number
+  setList: { nomeMusica: string; tempoMusica: number; letraMusica: string }[]
+  contato: {
+    email: string
+    nomePrimeiroNumero: string
+    primeiroNumero: string
+    nomeSegundoNumero: string
+    segundoNumero: string
+  }
+}
+export interface BandProps {
+  params: { id: string }
+}
+export interface IdBand {
+  id: string
+  banda: string
+}
