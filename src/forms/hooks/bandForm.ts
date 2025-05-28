3e6ac298f1ad455d4e0191cefdc499c115b03eaf
J27mendez <mendes_j27@hotmail.com>
2025-05-28 11:43:25 -0300

    refactor: add mode onSubmit to useBandForm

--- src/forms/hooks/bandForm.ts
+++ src/forms/hooks/bandForm.ts
@@ -7,6 +7,7 @@ import { CreateBandFormInput } from "@/types"
 export const useBandForm = () => {
   const bandForm = useForm<CreateBandFormInput>({
     resolver: zodResolver(createBandFormSchema),
+    mode: "onSubmit",
     defaultValues: {
       banda: "",
       quantidadeIntegrantes: 1,
