ad98bdfe63ffc7cbc61fd322da304b35d59c5e7f
J27mendez <mendes_j27@hotmail.com>
2025-05-28 13:10:58 -0300

    feat: add component ReleaseTextarea

---
+++ src/components/Release.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useFormContext } from "react-hook-form"
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import { Textarea } from "./ui/textarea"
+
+const ReleaseTextarea = () => {
+  const { control } = useFormContext()
+
+  return (
+    <FormField
+      control={control}
+      name="release"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel htmlFor="release" className="font-semibold text-white">
+            Release
+          </FormLabel>
+          <FormControl>
+            <Textarea
+              id="release"
+              placeholder="Conte um pouco sobre a banda..."
+              {...field}
+              className="bg-transparent text-xl text-white placeholder:text-xl placeholder:text-white"
+              rows={5}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default ReleaseTextarea
