f8d396d730972d8a50b5085409f5483f7ac6dfd0
J27mendez <mendes_j27@hotmail.com>
2025-05-28 13:10:24 -0300

    feat: add component EstiloInput

---
+++ src/components/Estilo.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useFormContext } from "react-hook-form"
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import { Input } from "./ui/input"
+
+const EstiloInput = () => {
+  const { control } = useFormContext()
+
+  return (
+    <FormField
+      control={control}
+      name="estilo"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel htmlFor="estilo" className="font-semibold text-white">
+            Estilo da Banda
+          </FormLabel>
+          <FormControl>
+            <Input
+              id="estilo"
+              placeholder="Ex: Rock n' Roll"
+              {...field}
+              className="bg-transparent text-xl text-white placeholder:text-white"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default EstiloInput
