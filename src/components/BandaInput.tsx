47ae53ec111dec05a360a30ed40acd8ad218f589
J27mendez <mendes_j27@hotmail.com>
2025-05-28 11:42:37 -0300

    feat: add BandaInput component

---
+++ src/components/BandaInput.tsx
@@ -0,0 +1,39 @@
+import { useFormContext } from "react-hook-form"
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import { Input } from "./ui/input"
+
+const BandaInput = () => {
+  const { control } = useFormContext()
+
+  return (
+    <FormField
+      control={control}
+      name="banda"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel htmlFor="banda" className="font-semibold text-white">
+            Nome da Banda
+          </FormLabel>
+          <FormControl>
+            <Input
+              id="banda"
+              placeholder="Ex: Rock & Exploration"
+              {...field}
+              className="bg-transparent text-xl text-white placeholder:text-white"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default BandaInput
